c47f4b9d0d0503488335a87bd50c049ce41aa9b2
zhanghaichang <zhanghaichang@163.com>
2018-10-01 09:35:56 +0800

    Create clean.sh

---
+++ docker/clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+docker rm -f $(docker ps -qa)
+rm -rf /var/etcd/
+for m in $(tac /proc/mounts | awk '{print $2}' | grep /var/lib/kubelet); do
+    umount $m || true
+done
+rm -rf /var/lib/kubelet/
+for m in $(tac /proc/mounts | awk '{print $2}' | grep /var/lib/rancher); do
+    umount $m || true
+done
+rm -rf /var/lib/rancher/
+rm -rf /run/kubernetes/
+rm -rf /etc/kubernetes/
+rm -rf /opt/cni/bin/
+docker volume rm $(docker volume ls -q)
+docker ps -a
