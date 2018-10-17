4090f790c613db32b7e9609dc689c9d763404b34
zhanghaichang <zhanghaichang@163.com>
2018-10-17 23:06:15 +0800

    Create rancher-clean.sh

---
+++ rancher/rancher-clean.sh
@@ -0,0 +1,19 @@
+docker rm -f $(docker ps -qa)
+  docker volume rm $(docker volume ls -q)
+  for mount in $(mount | grep tmpfs | grep '/var/lib/kubelet' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
+  rm -rf /etc/ceph \
+       /etc/cni \
+       /etc/kubernetes \
+       /opt/cni \
+       /opt/rke \
+       /run/secrets/kubernetes.io \
+       /run/calico \
+       /run/flannel \
+       /var/lib/calico \
+       /var/lib/etcd \
+       /var/lib/cni \
+       /var/lib/kubelet \
+       /var/lib/rancher \
+       /var/log/containers \
+       /var/log/pods \
+       /var/run/calico
