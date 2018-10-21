f82fac71fc13a5602627164a529a542b7d932b53
zhanghaichang <zhanghaichang@163.com>
2018-10-22 01:20:56 +0800

    Update clean.sh

--- docker/clean.sh
+++ docker/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker rm -f $(docker ps -qa)
+docker rm -f -v $(docker ps -aq)
 rm -rf /var/etcd/
 for m in $(tac /proc/mounts | awk '{print $2}' | grep /var/lib/kubelet); do
     umount $m || true
@@ -12,5 +12,6 @@ rm -rf /var/lib/rancher/
 rm -rf /run/kubernetes/
 rm -rf /etc/kubernetes/
 rm -rf /opt/cni/bin/
+ rm -rf /var/run/calico
 docker volume rm $(docker volume ls -q)
 docker ps -a
